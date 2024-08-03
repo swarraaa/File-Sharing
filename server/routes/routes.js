e1198a31162b5766a7a73122da63ab03c8d73814
swarraaa <swaradapb113@gmail.com>
2024-08-03 17:53:58 +0530

    Containerised

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,14 @@
-const express = require('express')
-const { uploadImage, downloadImage } = require('../controller/image-controller')
-const router = express.Router()
-const upload = require('../utils/upload')
+const express = require("express");
+const {
+  uploadImage,
+  downloadImage,
+} = require("../controller/image-controller");
+const router = express.Router();
+const upload = require("../utils/upload");
 
-router.post('/upload', upload.single('file'), uploadImage)
-router.get('/file/:fileId', downloadImage)
-
-module.exports = router
+router.post("/upload", upload.single("file"), uploadImage);
+router.get("/file/:fileId", downloadImage);
+router.get("/health", (req, res) => {
+  res.status(200).send("Server is running");
+});
+module.exports = router;
